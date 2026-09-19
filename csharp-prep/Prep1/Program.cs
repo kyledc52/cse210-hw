92ab063dc8cfa9632ed4381704904f4ef11317b4
Kyle <kyledc52@gmail.com>
2026-09-19 13:57:09 -0600

    completed prep1 and prep2 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Whats is your first name?");
+        String firstName = Console.ReadLine();
+        Console.WriteLine("Whats is your last name?");
+        String lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
