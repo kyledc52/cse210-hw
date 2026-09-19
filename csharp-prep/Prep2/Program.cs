f0acd4a76f22f3f99d5d5e48f6cae971998f1d76
Kyle <kyledc52@gmail.com>
2026-09-19 13:58:36 -0600

    completed prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter Grade:");
+        String userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+        String gradeAchieved;
+        if (grade >= 90)
+        {
+            gradeAchieved = "A";
+        }
+        else if (grade >= 80)
+        {
+            gradeAchieved = "B";
+        }
+        else if (grade >= 70)
+        {
+            gradeAchieved = "C";
+        }
+        else if (grade >= 60)
+        {
+            gradeAchieved = "D";
+        }
+        else
+        {
+            gradeAchieved = "F";
+        }
+        Console.WriteLine($"Grade: {gradeAchieved}");
     }
 }
