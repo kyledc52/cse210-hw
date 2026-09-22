be444821d56ad1382aae5eb6d7d296eeb41313ec
Kyle <kyledc52@gmail.com>
2026-09-21 19:22:14 -0600

    Did prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number: ");
+        String magicNumberUser = Console.ReadLine();
+        int magicNumber = int.Parse(magicNumberUser);
+
+        // Loop til they guess magic number
+        String response;
+        do
+        {
+            Console.WriteLine("What is your guess?: ");
+            response = Console.ReadLine();
+            if (int.Parse(response) < magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (int.Parse(response) > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+        }   while (int.Parse(response) != magicNumber);
+
+        Console.WriteLine("You guessed it!");
     }
 }
