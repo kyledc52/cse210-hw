1b4a0003f7bdc2f93f3da4d3929d9a03292e6d31
Kyle <kyledc52@gmail.com>
2026-09-23 16:28:57 -0600

    Did prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        String n;
+        List<int> numbers = new List<int>();
+
+        // get list
+        while(true)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            n = Console.ReadLine();
+            if (int.Parse(n) == 0)
+            {
+                break;
+            }
+            else
+            {
+                // add to list
+                numbers.Add(int.Parse(n));
+            }
+        }
+
+        // get sum
+        int sum = 0;
+        for (int i = 0; i < numbers.Count(); i++)
+        {
+            sum += numbers[i];
+        }
+        Console.WriteLine($"Sum: {sum}.");
+
+        // get average 
+        double average = (double)sum / numbers.Count();
+        Console.WriteLine($"The average is {average}.");
+
+        // get largest number
+        int largestNumber = numbers.Max();
+        Console.WriteLine($"The largest number is {largestNumber}.");
     }
 }
